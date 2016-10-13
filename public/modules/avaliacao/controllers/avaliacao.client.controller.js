601e684808c27d505444cba187f48288d44b8656
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2016-10-13 18:02:00 -0300

    Adicionado o combo com a opção de selecionar as divisões cadastradas pelo sistema

--- public/modules/avaliacao/controllers/avaliacao.client.controller.js
+++ public/modules/avaliacao/controllers/avaliacao.client.controller.js
@@ -1,52 +1,52 @@
 'use strict';
 
-angular.module('avaliacao').controller('AvaliacaoController', ['$scope', '$stateParams', '$location', 'Authentication', 'Avaliacao' ,'Divisao',
-	function($scope, $stateParams, $location, Authentication, Avaliacao, Divisao) {
-		
-		$scope.authentication = Authentication;
-	  	$scope.currentPage = 1;
-	  	$scope.pageSize = 10;
-	  	$scope.offset = 0;
+angular.module('avaliacao').controller('AvaliacaoController', ['$scope', '$stateParams', '$location', 'Authentication', 'Avaliacao', 'Divisao',
+	function ($scope, $stateParams, $location, Authentication, Avaliacao, Divisao) {
 
-	   // Page changed handler
-	   $scope.pageChanged = function() {
-	  		$scope.offset = ($scope.currentPage - 1) * $scope.pageSize;
-	   };		  
+		$scope.authentication = Authentication;
+		$scope.currentPage = 1;
+		$scope.pageSize = 10;
+		$scope.offset = 0;
+
+		// Page changed handler
+		$scope.pageChanged = function () {
+			$scope.offset = ($scope.currentPage - 1) * $scope.pageSize;
+		};
 
 		// Create new Avaliação
-		$scope.create = function() {
+		$scope.create = function () {
 			// Create new Avaliação object
-			var avaliacao = new Avaliacao ({
+			var avaliacao = new Avaliacao({
 				name: this.name,
 				description: this.description
 			});
 
 			// Redirect after save
-			avaliacao.$save(function(response) {
+			avaliacao.$save(function (response) {
 				$location.path('avaliacao/' + response._id);
 
 				// Clear form fields
 				$scope.name = '';
-			}, function(errorResponse) {
+			}, function (errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
 		};
 
 		// Remove existing Avaliacao
-		$scope.remove = function(avaliacao) {
-			if ( avaliacao ) { 
-				avaliacao.$remove(function(response) {
+		$scope.remove = function (avaliacao) {
+			if (avaliacao) {
+				avaliacao.$remove(function (response) {
 					$scope.success = response.message;
 					console.log(response.message);
 					//$location.path('avaliacao');
 				});
 				for (var i in $scope.avaliacao) {
-					if ($scope.avaliacao [i] === avaliacao) {
+					if ($scope.avaliacao[i] === avaliacao) {
 						$scope.avaliacao.splice(i, 1);
 					}
 				}
 			} else {
-				$scope.avaliacao.$remove(function(response) {
+				$scope.avaliacao.$remove(function (response) {
 					$scope.success = response.message;
 					//$location.path('avaliacao');
 				});
@@ -54,45 +54,41 @@ angular.module('avaliacao').controller('AvaliacaoController', ['$scope', '$state
 		};
 
 		// Update existing Avaliacao
-		$scope.update = function() {
+		$scope.update = function () {
 			var avaliacao = $scope.avaliacao;
-			
-			avaliacao.$update(function(response) {
+
+			avaliacao.$update(function (response) {
 				console.log(response);
 				$location.path('avaliacao/' + response.identificador);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;	
+			}, function (errorResponse) {
+				$scope.error = errorResponse.data.message;
 			});
-		};		
+		};
 
 		// Find a list of Avaliacao
-		$scope.find = function() {
+		$scope.find = function () {
 			$scope.avaliacao = Avaliacao.query();
 		};
 
-		$scope.findDivisao = function() {
-			$scope.divisao = Divisao.query();
-			
-			for (var i in $scope.divisao) {
-					//if ($scope.avaliacao [i] === $scope.divisao) {
-						console.log($scope.avaliacao[i]);
-					//}
-				}
-			//$scope.divisao = Divisao.query();
-			//$scope.divisao = $scope.divisao;
+		// Find a list of Division
+		$scope.findDivisao = function () {
+			$scope.divisoes = Divisao.query();
+			// for (var i in $scope.divisao) {
+			// 	console.log($scope.divisao[i].$$state);
+			// }
 		};
 
 		// Find existing Avaliação
-		$scope.findOne = function() {
-			$scope.avaliacao = Avaliacao.get({ 
+		$scope.findOne = function () {
+			$scope.avaliacao = Avaliacao.get({
 				avaliacaoId: $stateParams.avaliacaoId
 			});
 		};
 
 		// Search for a Avaliação
-		$scope.categorySearch = function(product) {
+		$scope.categorySearch = function (product) {
 			$location.path('avaliacao/' + product._id);
-		};		
+		};
 
 
 
